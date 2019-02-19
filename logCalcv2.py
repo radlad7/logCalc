fafa21cb7697fd7b07b6c21aee45111dbeff3e88
Ray <radlad@protonmail.com>
2019-02-18 20:56:38 -0500

    logCalc initially created 11/4/18. Update on 2/18/19 includes WIP prototypes for User Prompt, Base 2 Pre-Filled Example. Presentation of results overhauled, current default testing shows logarithm calculator examples mostly in base 2.

---
+++ logCalcv2.py
@@ -0,0 +1,61 @@
+# Another way of explaining log2 8 is:
+# if we kept dividing 8 by 2 until we ended up with 1,
+# how many 2s would we have in our equation?
+
+def logCount(base, number):
+	result = number // base
+	count = 0
+
+	print("\n\nlog of {} to base {}".format(number, base))
+	print("*************************")
+	print("\n{}/{} = {}".format(number, base, result))
+	count +=1
+
+	while result != 1:
+		result1 = result
+		result = result // base
+		print("{}/{} = {}".format(result1, base, result))
+		count +=1
+		
+		if result <= 1:
+			print("\n     Final Result: \n\nlog base ({}) of {} = \n\n\t>> {} <<".format(base, number, count))
+			break
+	print("*************************")
+
+
+# Log Calc Prompt
+'''
+print("\n********************\nWelcome to Ray's Log Calc.\n\nThis tool can help calculate common (base 10) and binary (base 2) logarithms.\n\nCurrently fractions, negative numbers and bases outside of 2 and 10 are not supported.Apologies to the Ph.D mathematicians out there.\n********************\n")
+baseNum = int(input("Please enter a base number: "))
+logOfNum = int(input("Now enter the desired logarithm of x to base {}: ".format(baseNum)))
+logCount(baseNum, logOfNum)
+# print("you have selected: " + logCount(baseNum, logOfNum))
+# logCount()
+'''
+
+# Base 2 Table for Pre-Filled Example - WIP
+'''
+i = 2
+result = 0
+while result < 8388608:
+	result = i * 2
+	i = i * 2
+	logCount(i, result)
+	print(logCount)
+	print(result)
+'''
+
+# Testing Log Calc in Base 2, 10 and 5.
+logCount(2,8)
+logCount(2, 16)
+logCount(2, 32)
+logCount(2, 64)
+logCount(2, 128)
+logCount(2, 256)
+logCount(2, 512)
+logCount(2, 1024)
+logCount(2, 2048)
+
+logCount(10, 100)
+logCount(2, 24)
+logCount(5, 625)
